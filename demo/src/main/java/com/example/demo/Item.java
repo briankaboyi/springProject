fbfcb7232b258c803e2a2043df9409c7eca135b6
briankaboyi <brianmkaboyi@gmail.com>
2024-01-26 11:09:15 +0300

    entity service repo controller

--- demo/src/main/java/com/example/demo/Item.java
+++ demo/src/main/java/com/example/demo/Item.java
@@ -1,6 +1,6 @@
 package com.example.demo;
 
-import java.persistence.Entity;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
